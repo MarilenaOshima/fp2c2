d7fc5c2bca518726e1a12fd5d70929a473b3209d
aluno <aluno@LAB06M01>
2022-02-25 15:13:25 -0300

    Projeto inicial.

---
+++ src/java/br/edu/ifsp/pep/controller/ExemploController.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.controller;
+
+import br.edu.ifsp.pep.model.Pessoa;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author aluno
+ */
+//arquitetura:
+// view (.xhtml) --> controller (managed bean) --> model (entity)
+
+// pra determinar uma classe managed bean
+// precisa ter o @Named, o escopo e um contrutor sem parametro
+// import biblioteca java ee 7
+@Named
+// determina o escopo
+
+//@RequestScoped
+// escopo do tipo request, a cada requisição faz uma nova instância.
+// cria toda vez uma nova instância
+//toda vez que fizer uma chamada, vai criar uma nova instância praquela classe
+//no caso quem cria é o glassfish
+//chamada de container...o servidor é chamada de container
+//quem gerencia o managed bean é o container
+//se você quiser mandar informções..não pode ser do tipo @RequestScoped
+//pq toda vez instância um novo objeo...aí zer a lista e cria uma lista vazia toda vez
+//por isso não dá para guardar informações
+
+@ViewScoped
+/**
+precisa implementar na classe Serializable
+continua precisando do @Named e construtor sem parametro
+a instância será a mesma enquanto estiver na mesma página
+esse escopo serve qnd vc precisa por exemplo add itens e exibir na mesma página
+
+*/
+public class ExemploController implements Serializable {
+
+    private Pessoa pessoa;   
+    private List<Pessoa> pessoas = new ArrayList<>();
+
+    //instância o atributo
+    public ExemploController() {
+        System.out.println("Construtor.");
+        this.pessoa = new Pessoa();
+    }
+
+    public void adicionar() {
+        System.out.println("Executou o método teste.");
+        System.out.println("Nome: " + this.pessoa.getNome());
+        this.pessoas.add(pessoa);
+        this.pessoa = new Pessoa();
+        addMessage(FacesMessage.SEVERITY_INFO, "Informação", "Cadastro realizado.");
+    }
+
+    private void addMessage(FacesMessage.Severity severity, String summary, String detail) {
+        FacesContext.getCurrentInstance().
+                addMessage(null, new FacesMessage(severity, summary, detail));
+    }
+
+    /*
+    public void remover () {
+        this.pessoas.remove(pessoa);
+    }*/
+
+    public void exibir() {
+        for (Pessoa p : pessoas) {
+            System.out.println(p.getNome());
+        }
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+
+
+    
+
+}
---
+++ src/java/br/edu/ifsp/pep/model/Pessoa.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Pessoa {
+
+    private String nome;
+    private Date dataNascimento;
+    private boolean status;
+    private String uf;
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+}
